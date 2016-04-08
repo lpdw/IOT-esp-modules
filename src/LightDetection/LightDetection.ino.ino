fb5d238f42b6dc26766000bcbccc4f82066e1aac
AuroreT <aurore.troubat@gmail.com>
2016-04-08 14:48:36 +0200

    Add light detection module

---
+++ src/LightDetection/LightDetection.ino.ino
@@ -0,0 +1,74 @@
+/*
+ http://www.arduino.cc/en/Tutorial/AnalogInput
+ */
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int crossingTrain[5] = {0};
+int i = 0;
+int itrArray = 0;
+bool flag = true;
+double moyenneLuminosite = 0.0;
+
+void setup() {
+  Serial.begin(115200);
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+//Clean un tableau d'entiers
+void reinitArray(int arrayToClean[], int size){
+  for(int p = 0; p <= size; p++)
+  {
+    arrayToClean[p] = 0; 
+  }
+}
+
+//Calcul la moyenne 
+double moyenneTableau(int tableau[], int tailleTableau){
+    int j = 0, somme = 0;
+    double moyenne = 0;
+ 
+    for(j = 0; j < tailleTableau; j++)
+    {
+        somme = somme + tableau[j];
+    }
+ 
+    moyenne = somme / j;
+
+    return moyenne;
+}
+
+void reinitFlag (){
+    flag = false;
+    delay(3000);
+    flag = true;
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+
+  //If the train is there, push value in crossingTrain array 
+  if(sensorValue < 1030 && sensorValue > 900 && itrArray <= 5){
+    crossingTrain[itrArray] = sensorValue;
+    itrArray++;
+
+    if(itrArray == 5){
+      moyenneLuminosite = moyenneTableau(crossingTrain, 5);
+
+      if(moyenneLuminosite <1000 && moyenneLuminosite > 900 && flag == true){
+        Serial.println("TRAIN PASSE");
+        digitalWrite(ledPin, HIGH);
+        reinitFlag();
+      }
+      reinitArray(crossingTrain, 5);
+      itrArray = 0;
+    }
+  } 
+  else{
+    digitalWrite(ledPin, LOW);
+  }
+  delay(5);
+}
