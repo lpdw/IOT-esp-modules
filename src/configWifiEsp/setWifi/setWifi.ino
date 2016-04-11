d7134fab21e0ae6e30532ecc4b9f4ae4e883dbc7
ThibautArnoud <thibaut.arnoud@gmail.com>
2016-04-11 10:24:13 +0200

    add comments

--- src/configWifiEsp/setWifi/setWifi.ino
+++ src/configWifiEsp/setWifi/setWifi.ino
@@ -9,9 +9,14 @@ bool isServer = false;
 
 char ssid[] = "SSIDEXEMPLEMODULE";
 char password[] = "PASSWORDEXEMPLEMODULE";
+String uuid;
+String type;
+String label;
+String ipHub;
 
 ESP8266WebServer server(80);
 
+/*Open file config*/
 bool readConfig(){
   File f = SPIFFS.open("/config.txt", "r");
   if (!f) {
@@ -24,6 +29,7 @@ bool readConfig(){
       Serial.println("Line : " + i);
       Serial.println(s);
     }
+    /*Check if line 1 and 2 (SSID and Password) are not missing*/
     if(config[0].length() == 0 || config[1].length() == 0 ){
       Serial.println("Config not ok");
       return false;
@@ -33,6 +39,7 @@ bool readConfig(){
   }
 }
 
+/*Configuring Acces point with unique SSID*/
 void setWifiAccesPoint(){
   Serial.println("Configuring access point...");
   WiFi.disconnect();
@@ -43,6 +50,7 @@ void setWifiAccesPoint(){
   Serial.println(myIP);
 }
 
+/* Configuring http server and listen for POST request on /configwifi */
 void setServer(){
   isServer = true;
   server.on("/configwifi", HTTP_POST, handleConfig);
@@ -50,39 +58,39 @@ void setServer(){
   Serial.println("HTTP server started");
 }
 
+/* Handling POST request on /configwifi */
 void handleConfig(){
   int args = server.args();
   String requestData;
+  /*If request params are not SSID or PASSWORD return HTTP error 400 */
   if(server.argName(0) != "ssid" || server.argName(1) != "password" ){
     server.send(400, "text/plain", "Arguments must be \"ssid\" or \"password\"");
   }else{
-  for (uint8_t i = 0; i < server.args(); i++) {
-    String param = server.argName(i);
-    requestData += " " + param + ": " + server.arg(i) + "\n";
-  }
-  Serial.println(requestData);
-  String __ssid = server.arg(0);
-  String __password = server.arg(1);
-  __ssid.toCharArray(ssid, __ssid.length()+1);
-  Serial.println("*******");
-  Serial.println(ssid);
-  __password.toCharArray(password, __password.length()+1);
-  Serial.println("--------");
-  Serial.println(ssid);
-  Serial.println(password);
-  Serial.println("--------");
-  server.send(200, "text/plain", requestData);
-  setWifiClient();
+    /* if everything is OK, send response 200 with the data pass with the request*/
+    for (uint8_t i = 0; i < server.args(); i++) {
+      String param = server.argName(i);
+      requestData += " " + param + ": " + server.arg(i) + "\n";
+    }
+    server.send(200, "text/plain", requestData);
+    Serial.println(requestData);
+    /* Store request params with  temporary varaibles*/
+    String __ssid = server.arg(0);
+    String __password = server.arg(1);
+    /* Convert String to char array because wifi.begin() method accept only char array */
+    __ssid.toCharArray(ssid, __ssid.length()+1);
+    __password.toCharArray(password, __password.length()+1);
+    setWifiClient();
   }
 }
 
+/*Connect module to hub with the data stored before*/
 void setWifiClient(){
   Serial.println("Set client");
   WiFi.disconnect();
   delay(100);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
+  
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
