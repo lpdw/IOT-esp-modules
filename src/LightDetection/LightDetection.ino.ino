713b93d4912a270a247325c359931a9c79e470db
Aurore Troubat <AuroreT@users.noreply.github.com>
2016-04-12 15:47:10 +0200

    Add Post request

--- src/LightDetection/LightDetection.ino.ino
+++ src/LightDetection/LightDetection.ino.ino
@@ -1,7 +1,12 @@
-/*
- http://www.arduino.cc/en/Tutorial/AnalogInput
- */
+#include <Arduino.h>
 
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
@@ -10,11 +15,20 @@ int i = 0;
 int itrArray = 0;
 bool flag = true;
 double moyenneLuminosite = 0.0;
+const char* sensorID = "123456789";
 
 void setup() {
   Serial.begin(115200);
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
+
+  for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFiMulti.addAP("LPDW-IOT", "LPDWIOTROUTER2015");
 }
 
 //Clean un tableau d'entiers
@@ -59,8 +73,23 @@ void loop() {
       moyenneLuminosite = moyenneTableau(crossingTrain, 5);
 
       if(moyenneLuminosite <1000 && moyenneLuminosite > 900 && flag == true){
-        Serial.println("TRAIN PASSE");
         digitalWrite(ledPin, HIGH);
+
+        if((WiFiMulti.run() == WL_CONNECTED)) {
+        
+                HTTPClient http;
+                USE_SERIAL.print("[HTTP] begin...\n");
+                // configure traged server and url
+                http.begin("http://192.168.1.222/data"); //HTTP
+        
+                USE_SERIAL.print("[HTTP] POST...\n");
+                // start connection and send HTTP header
+                http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+                //http.POST("title=foo&value=1");
+                http.POST(sensorID);
+                http.writeToStream(&Serial);
+                http.end();
+            }
         reinitFlag();
       }
       reinitArray(crossingTrain, 5);
